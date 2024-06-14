cb76bf171a6040eb9133ca77f750b0db7ff088f5
Shreyas Veturi <veturi.s@etoncollege.org.uk>
2024-06-14 09:48:56 +0100

    Initial Commit

---
+++ Cloud Storage/Cloud_StorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cloud_StorageApp.swift
+//  Cloud Storage
+//
+//  Created by Veturi, Shreyas (HJRM) on 14/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Cloud_StorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
